8e55dd5d1ea53f272ec8a659473f77124c053abf
OleksiiTreninbc2024-3 <Oleksii.trenin@lnu.edu.ua>
2024-10-21 13:07:11 +0300

    f

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const fs = require('fs').promises;
 const { Command } = require('commander');
 const superagent = require('superagent');
 
+
 const program = new Command();
 program
   .option('-h, --host <type>', 'адреса сервера')
@@ -12,11 +13,8 @@ program
 
 program.parse(process.argv);
 
-const options = program.opts();
-const cacheDir = path.resolve(options.cache);
-const host = options.host;
-const port = options.port;
-const cache = options.cache;
+const { host, port, cache } = program.opts();
+const cacheDir = path.resolve(cache);
 
 if (!host || !port || !cache) {
   console.error('Помилка: не задано обов`язкові параметри --host, --port та --cache.');
@@ -32,10 +30,12 @@ async function fetchImageFromHttpCat(statusCode) {
   }
 }
 
+
 const server = http.createServer(async (req, res) => {
   const statusCode = req.url.slice(1);
   const filePath = path.join(cacheDir, `${statusCode}.jpg`);
 
+ 
   if (req.method === 'GET') {
     try {
       const fileData = await fs.readFile(filePath);
@@ -52,7 +52,9 @@ const server = http.createServer(async (req, res) => {
         res.end('Not Found');
       }
     }
-  } else if (req.method === 'PUT') {
+  } 
+
+  else if (req.method === 'PUT') {
     let body = [];
     req.on('data', chunk => body.push(chunk));
     req.on('end', async () => {
@@ -66,7 +68,9 @@ const server = http.createServer(async (req, res) => {
         res.end('Internal server error');
       }
     });
-  } else if (req.method === 'DELETE') {
+  } 
+  
+  else if (req.method === 'DELETE') {
     try {
       await fs.unlink(filePath);
       res.writeHead(200, { 'Content-Type': 'text/plain' });
@@ -75,12 +79,15 @@ const server = http.createServer(async (req, res) => {
       res.writeHead(404, { 'Content-Type': 'text/plain' });
       res.end('Not Found');
     }
-  } else {
+  } 
+ 
+  else {
     res.writeHead(405, { 'Content-Type': 'text/plain' });
     res.end('Method not allowed');
   }
 });
 
-server.listen(options.port, options.host, () => {
-  console.log(`Сервер запущено на http://${options.host}:${options.port}`);
+
+server.listen(port, host, () => {
+  console.log(`Сервер запущено на http://${host}:${port}`);
 });
